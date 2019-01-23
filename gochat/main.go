7dcadde8036fe1881ae489ae27407e1622583aad
Zhan9YunHua <zy.hua1122@qq.com>
2019-01-23 20:01:45 +0800

    conf

--- gochat/main.go
+++ gochat/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"time"
 
 	"github.com/Zhan9Yunhua/gochat"
 )
 
 func main() {
+	name := flag.String("n", "", "name")
+	cont := flag.String("c", "", "content")
+	second := flag.Int("s", 10, "second")
+
+	flag.Parse()
+
+	if *name == "" {
+		log.Fatalln("-n (昵称)不能为空")
+	}
+
+	if *cont == "" {
+		log.Fatalln("-c (内容)不能为空")
+	}
+
 	c, _ := gochat.NewChat(&gochat.Chat{
 		QrcodeProt:   8005,
 		IsQrcodeFile: false,
@@ -15,9 +30,10 @@ func main() {
 	c.Start()
 
 	// ！昵称！毕竟有些没备注
-	nickName := "微信昵称"
+	// nickName := "微信昵称"
 	for {
-		c.SendMessage(nickName, fmt.Sprintf("## [test]  send a message to %s ## ---> %s", nickName, time.Now()))
-		time.Sleep(time.Second * 10)
+		// c.SendMessage(nickName, fmt.Sprintf("## [test]  send a message to %s ## ---> %s", nickName, time.Now()))
+		c.SendMessage(*name, *cont)
+		time.Sleep(time.Second * time.Duration(*second))
 	}
 }
